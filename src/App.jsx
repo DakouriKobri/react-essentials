cb00273d827b4b504a2aa4e089a8855f78f6e294
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-05-03 09:40:32 +0200

    Add image to Header dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// Project Imports
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDescription = ['Fundamental', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
@@ -8,7 +11,7 @@ function Header() {
   const description = reactDescription[getRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
