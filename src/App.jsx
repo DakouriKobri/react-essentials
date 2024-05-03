53c90e767a03d114ad3bce582292e46d654f34c8
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-05-03 13:09:22 +0200

    Pass handleSeclect function as a prop value

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import TabButton from './components/tab-button';
 import { CORE_CONCEPTS } from './data';
 
 function App() {
+  function handleSelect() {
+    console.log('Hello World! - selected');
+  }
+
   return (
     <div>
       <Header />
@@ -22,10 +26,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
       </main>
